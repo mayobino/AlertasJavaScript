a2a522d88fa6e4fe33ec581db4336f117290ffc5
mayobino <obinomayra@gmail.com>
2021-11-04 15:51:14 -0300

    Se crean alertas con JS

---
+++ index.js
@@ -0,0 +1,14 @@
+
+let nombreDeLaUsuaria = prompt ("HOLA! Decinos tu nombre")
+let apellidoDeLaUsuaria = prompt ("Gracias" + " " + nombreDeLaUsuaria +", ahora dejanos tu apellido")
+
+alert ("Ahora sí! Hola " + nombreDeLaUsuaria + " " + apellidoDeLaUsuaria + ", ¡bienvenida a nuestro sitio!")
+
+let stringConComillasMagicas = `Hola, ${nombreDeLaUsuaria} ${apellidoDeLaUsuaria}, bienvenida a este sitio`
+console.log (stringConComillasMagicas)
+
+let edadDelUsuario = prompt ("Cuantos años tenes?")
+
+alert ("Naciste en el año: " + (2021 - edadDelUsuario))
+
+ 
